5e87b440728c6b883bfc86811f557fab63306a43
PCL-TUM <phichet.pcl@gmail.com>
2024-03-04 20:46:49 +0700

    add: pre and deloy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'profile-reactjs',
+  base: '/profile-reactjs/',
   plugins: [react()],
 })
